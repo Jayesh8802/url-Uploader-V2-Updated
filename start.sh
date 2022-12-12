cdba84aa4a092e92e77d12201b4815879adf540e
Jayesh8802 <114227844+Jayesh8802@users.noreply.github.com>
2022-12-12 21:57:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Teamofab/AllXUrlXUploader /LazyDeveloper
+git clone https://github.com/Jayesh8802/url-Uploader-V2-Updated /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
